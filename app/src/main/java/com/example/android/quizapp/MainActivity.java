3704503aa35b93665a0a1cffc98ed0d232bb102f
Ogbarrison <ogbarrison@gmail.com>
2018-07-01 05:44:39 +0100

    Committing this for more readability of my code. Tried getting the logic under 200 lines

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -15,8 +15,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // variable holding the candidate name
     private String candidateName;
+    // variable holding the total score of the candidate
     private int scoreCount;
+    // variable holding the number of TOTAL ATTEMPTED questions by the candidate
     private int totalQuestions;
 
     @Override
@@ -26,10 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method creates a summary of the order.
-     * <p>
-     * param view of the order
-     * return text summary
+     * This method captures the users name before they can get access to the questions.
      */
     public void registerCandidate(View view) {
         TextView nameView = findViewById(R.id.nameTextView);
@@ -48,13 +48,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Please register your NAME to continue.", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * This is a method to restart the app.
+     */
     public void restartApp(View view) {
         Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
     }
 
+    /**
+     * This method captures all input from the user for calculation and grading and displays the final score.
+     */
     public void submitAnswers(View view) {
+        // get all relevant data for RadioButton related questions
         RadioGroup radioGroupQ1 = findViewById(R.id.radioGroupQ1);
         int rightAnswerQ1 = R.id.optionQ1_b;
         RadioGroup radioGroupQ2 = findViewById(R.id.radioGroupQ2);
@@ -66,9 +73,11 @@ public class MainActivity extends AppCompatActivity {
         RadioGroup radioGroupQ8 = findViewById(R.id.radioGroupQ8);
         int rightAnswerQ8 = R.id.optionQ8_c;
 
+        // get all relevant data for EditText related questions
         EditText candidateAnswerTViewQ5 = findViewById(R.id.optionQ5_a);
         String rightAnswerQ5 = getResources().getString(R.string.q5_option_a);
 
+        // get all relevant data for CheckBox related questions
         CheckBox rightAnswer1Q4 = findViewById(R.id.optionQ4_a);
         CheckBox rightAnswer2Q4 = findViewById(R.id.optionQ4_d);
         CheckBox wrongAnswer1Q4 = findViewById(R.id.optionQ4_b);
@@ -78,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox wrongAnswer1Q6 = findViewById(R.id.optionQ6_b);
         CheckBox wrongAnswer2Q6 = findViewById(R.id.optionQ6_d);
 
+        // send the data to the relevant grade methods for scoring
         gradeRadioQuestions(radioGroupQ1, rightAnswerQ1);
         gradeRadioQuestions(radioGroupQ2, rightAnswerQ2);
         gradeRadioQuestions(radioGroupQ3, rightAnswerQ3);
@@ -89,11 +99,21 @@ public class MainActivity extends AppCompatActivity {
         gradeCheckQuestions(rightAnswer1Q4, rightAnswer2Q4, wrongAnswer1Q4, wrongAnswer2Q4);
         gradeCheckQuestions(rightAnswer1Q6, rightAnswer2Q6, wrongAnswer1Q6, wrongAnswer2Q6);
 
-        Toast.makeText(this, "You got " + scoreCount + " ANSWERS RIGHT out of " + totalQuestions + " ATTEMPTED questions", Toast.LENGTH_SHORT).show();
+        // display the candidate score
+        Toast.makeText(this, "Hello " + candidateName + ", you got " + scoreCount + " ANSWERS RIGHT out of " + totalQuestions + " ATTEMPTED questions", Toast.LENGTH_LONG).show();
+
+        // reset the scoring data
         scoreCount = 0;
         totalQuestions = 0;
     }
 
+    /**
+     * This method calculates the score of all questions that require text answers.
+     * <p>
+     *
+     * @param candidateAnswerTView The TextView holding the candidate answer.
+     * @param rightAnswer          The correct answer to this question in String type.
+     */
     public void gradeTextQuestions(EditText candidateAnswerTView, String rightAnswer) {
         String candidateAnswer = candidateAnswerTView.getText().toString();
         if (candidateAnswer.trim().length() > 0) {
@@ -105,33 +125,49 @@ public class MainActivity extends AppCompatActivity {
                 candidateAnswerTView.setText(rightAnswer);
                 candidateAnswerTView.setTextColor(getResources().getColor(R.color.colorPrimaryText));
             }
-        } else {
-//            Toast.makeText(this, "Please answer all questions TEXT.", Toast.LENGTH_SHORT).show();
-//            return;
         }
     }
 
-    public void gradeCheckQuestions(CheckBox rightAnswer1Q4, CheckBox rightAnswer2Q4, CheckBox wrongAnswer1Q4, CheckBox wrongAnswer2Q4) {
-        if (wrongAnswer1Q4.isChecked() || wrongAnswer2Q4.isChecked()) {
+    /**
+     * This method calculates the score of all questions that require checkboxed answers.
+     * <p>
+     *
+     * @param rightAnswer1 The CheckBox holding the first correct answer.
+     * @param rightAnswer2 The CheckBox holding the second correct answer.
+     * @param wrongAnswer1 The CheckBox holding the first wrong answer.
+     * @param wrongAnswer2 The CheckBox holding the second wrong answer.
+     */
+    public void gradeCheckQuestions(CheckBox rightAnswer1, CheckBox rightAnswer2, CheckBox wrongAnswer1, CheckBox wrongAnswer2) {
+        if (wrongAnswer1.isChecked() || wrongAnswer2.isChecked()) {
             totalQuestions += 1;
-        } else if (rightAnswer1Q4.isChecked() && rightAnswer2Q4.isChecked()) {
+            wrongAnswer1.setChecked(false);
+            wrongAnswer2.setChecked(false);
+            rightAnswer1.setChecked(true);
+            rightAnswer2.setChecked(true);
+            rightAnswer1.setTextColor(getResources().getColor(R.color.colorPrimaryText));
+            rightAnswer2.setTextColor(getResources().getColor(R.color.colorPrimaryText));
+        } else if (rightAnswer1.isChecked() && rightAnswer2.isChecked()) {
             scoreCount += 1;
             totalQuestions += 1;
-        } else {
-//            Toast.makeText(this, "Please answer all questions CHECK.", Toast.LENGTH_SHORT).show();
+            wrongAnswer1.setChecked(false);
+            wrongAnswer2.setChecked(false);
+            rightAnswer1.setChecked(true);
+            rightAnswer2.setChecked(true);
+            rightAnswer1.setTextColor(getResources().getColor(R.color.colorPrimaryText));
+            rightAnswer2.setTextColor(getResources().getColor(R.color.colorPrimaryText));
         }
-
-        wrongAnswer1Q4.setChecked(false);
-        wrongAnswer2Q4.setChecked(false);
-        rightAnswer1Q4.setChecked(true);
-        rightAnswer2Q4.setChecked(true);
-        rightAnswer1Q4.setTextColor(getResources().getColor(R.color.colorPrimaryText));
-        rightAnswer2Q4.setTextColor(getResources().getColor(R.color.colorPrimaryText));
     }
 
+    /**
+     * This method calculates the score of all questions that require RadioButton answers.
+     * <p>
+     *
+     * @param radioGroup  The RadioGroup holding all the RadioButtons for the question.
+     * @param rightAnswer The int id of the RadioButton holding the correct answer.
+     */
     public void gradeRadioQuestions(RadioGroup radioGroup, int rightAnswer) {
         if (radioGroup.getCheckedRadioButtonId() == -1) {
-//            Toast.makeText(this, "Please answer all questions RADIO.", Toast.LENGTH_SHORT).show();
+            return;
         } else {
             RadioButton rightAnswerRadioButton = radioGroup.findViewById(rightAnswer);
             int yourAnswerRadioButtonID = radioGroup.getCheckedRadioButtonId();
